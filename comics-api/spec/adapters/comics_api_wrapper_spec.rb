2961e79287f15b248dd0c2995303d6175aed84b9
Marcel Ueno <marguera@gmail.com>
2022-08-05 15:34:00 -0300

    Add error condition to comics api wrapper spec

--- comics-api/spec/adapters/comics_api_wrapper_spec.rb
+++ comics-api/spec/adapters/comics_api_wrapper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "ComicsApiWrapper" do
 
   describe "find comics" do
+
     let(:api) {
       ComicsApiWrapper.new(
         public_key: "123",
@@ -10,26 +11,40 @@ RSpec.describe "ComicsApiWrapper" do
         timestamp: 1,
       )
     }
-   
-    before do
-      stub_request(:get, /#{ComicsApiWrapper::ENDPOINT}/)
-        .to_return({ body: '{"a":"b"}' })
-    end
 
-    it "appends the auth parameters" do
-      api.find
-      expect(a_request(:get, ComicsApiWrapper::ENDPOINT)
-        .with(query: { 
-          ts: '1', 
-          apikey: '123', 
-          hash: '41166ef71feca5c492e2dad09f42e685' 
-        })).to have_been_made 
-    end
+    let(:auth) {
+      { ts: '1', 
+        apikey: '123', 
+        hash: '41166ef71feca5c492e2dad09f42e685' }
+    }
 
-    it "returns the response string" do
-      response = api.find
-      expect(response).to be_a(String)
-      expect(response).to eq('{"a":"b"}')
+    context "with successfull request" do
+      before do
+        stub_request(:get, /#{ComicsApiWrapper::ENDPOINT}/)
+          .to_return({ body: '{"a":"b"}' })
+      end
+
+      it "appends the auth parameters" do
+        api.find
+        expect(a_request(:get, ComicsApiWrapper::ENDPOINT)
+          .with(query: auth)).to have_been_made 
+      end
+
+      it "returns the response string" do
+        expect(api.find).to be_a(String)
+        expect(api.find).to eq('{"a":"b"}')
+      end
+    end
+    
+    context "with invalid request" do
+      before do
+        stub_request(:get, /#{ComicsApiWrapper::ENDPOINT}/)
+          .to_raise("request error")
+      end
+
+      it "raises an error" do
+        expect { api.find }.to raise_error("request error")
+      end
     end
   end
 end
