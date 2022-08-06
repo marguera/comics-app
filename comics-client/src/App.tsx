7216217cb31dfb8c16f607b597853e1b2ab97ebb
Marcel Ueno <marguera@gmail.com>
2022-08-06 18:52:52 -0300

    Fetch and display the comics

--- comics-client/src/App.tsx
+++ comics-client/src/App.tsx
@@ -1,19 +1,50 @@
 import './App.scss';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { getComics, Comic } from './data'
 import Header from './components/Header';
 import SearchForm from './components/SearchForm';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App () {
+  const [comics, setComics] = useState<Comic[]>([]);
   const [character, setCharacter] = useState('Search for a character...');
 
+  const searchComics = (searchTerm: string): void => {
+    getComics(searchTerm).then(response => {
+      setComics(response);
+    });
+  };
+
   const onSubmit = (searchTerm: string) => {
+    searchComics(searchTerm);
     setCharacter(searchTerm);
   };
+
+  useEffect(() => {
+    searchComics("");
+  } , []);
 
   return (
     <div className="App">
       <Header />
       <SearchForm placeHolderText={character} onSubmit={onSubmit} />
+      <Container>
+        <h2>Search Results</h2>
+        <Row>
+          {comics.map(comic => (
+            <Col key={comic.id}>
+              <h3>{comic.title}</h3>
+              <img src={comic.thumbnail} alt={`${comic.title} cover`} />
+              <div>
+                <span>{comic.likes}</span>
+                <button></button>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   )
 }
---
+++ comics-client/src/data.ts
@@ -0,0 +1,12 @@
+export async function getComics(character: string): Promise<Comic[]> {
+  console.log('Fetching comics for ' + character);
+  const response = await fetch(`/api/v1/comics?character=${character}`);
+  return await response.json() as Comic[];
+}
+
+export type Comic = {
+    id: string
+    title: string
+    thumbnail: string
+    likes: number
+}
