ae2ac43b94984484d1f02c8293668f194499f0b6
Marcel Ueno <marguera@gmail.com>
2022-08-06 21:59:08 -0300

    Feat: Show likes count

--- comics-client/src/components/ComicLikes/ComicLikes.test.tsx
+++ comics-client/src/components/ComicLikes/ComicLikes.test.tsx
@@ -4,14 +4,19 @@ import { render, fireEvent } from '@testing-library/react';
 
 describe("ComicLikes", () => {
 
-  it("should be disabled if the comic is already liked", async () => {
-    const { findByRole } = render(<ComicLikes isLiked={true} />);
+  it("should be enabled if the comic is not liked", async () => {
+    const { findByRole } = render(<ComicLikes isLiked={false} onLikeClick={() => {}} />);
     const button = await findByRole("button");
-    console.log(button);
-    expect(button).toHaveAttribute('disabled');
+    expect(button).toBeEnabled();
   });
 
-  it("calls the callback function if given", () => {
+  it("should be disabled if the comic is already liked", async () => {
+    const { findByRole } = render(<ComicLikes isLiked={true} onLikeClick={() => {}} />);
+    const button = await findByRole("button");
+    expect(button).toBeDisabled();
+  });
+
+  it("calls the callback function", () => {
     const mockCallback = jest.fn();
     const { getByRole } = render(<ComicLikes isLiked={false} onLikeClick={mockCallback} />);
     const button = getByRole("button");
@@ -19,4 +24,22 @@ describe("ComicLikes", () => {
     expect(mockCallback.mock.calls.length).toBe(1);
   });
 
+  it("should not call the callback function", () => {
+    const mockCallback = jest.fn();
+    const { getByRole } = render(<ComicLikes isLiked={true} onLikeClick={mockCallback} />);
+    const button = getByRole("button");
+    fireEvent.click(button);
+    expect(mockCallback.mock.calls.length).toBe(0);
+  });
+  
+  it("shows the likes count", () => {
+    const mockCallback = jest.fn();
+    const { getByText } = render(
+      <ComicLikes 
+        likes={12345678} 
+        isLiked={true} 
+        onLikeClick={mockCallback} />);
+    const likesCount = getByText("12345678");
+    expect(likesCount).toBeInTheDocument();
+  });
 });
--- comics-client/src/components/ComicLikes/index.tsx
+++ comics-client/src/components/ComicLikes/index.tsx
@@ -2,31 +2,35 @@ import React, { useState, useEffect } from 'react';
 import styles from './ComicLikes.module.scss';
 
 interface ComicLikesProps {
-  isLiked: boolean
-  onLikeClick?: () => void
+  likes?: number;
+  isLiked: boolean;
+  onLikeClick: () => void;
 }
 
-export default function ComicLikes({ isLiked = false, onLikeClick }: ComicLikesProps) {
+export default function ComicLikes({ likes = 0, isLiked = false, onLikeClick }: ComicLikesProps) {
 
   const [ userLiked, setUserLiked ] = useState<boolean>(false);
+  const [ likesCount, setLikesCount ] = useState<number>(0);
 
   const handleClick = () => {
     if(onLikeClick && !userLiked) {
       setUserLiked(true);
+      setLikesCount(likesCount + 1);
       onLikeClick();
     }
-  }
+  };
 
   useEffect(() => {
+    setLikesCount(likes);
     setUserLiked(isLiked);
   }, []);
 
   return (
-    <div className="comic-likes">
+    <div className={styles.comicLikes} >
+      <span>{likesCount}</span>
       <button name="teste" 
         disabled={userLiked} 
         onClick={handleClick}
-        className={styles.comicLikes} 
       />
     </div>
   );
